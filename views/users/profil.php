bfb32618be459ddd1d6a3b0442e719cf44095c4b
Goudouland <fanny.cayzeele@gmail.com>
2014-08-19 17:47:45 +0200

    css page profil

--- views/users/profil.php
+++ views/users/profil.php
@@ -12,25 +12,59 @@
 <div class="profil-zone2">
     <div class="container">
         <img src="<?php echo WEBROOT . 'images/profil1.jpg'; ?>">
-        <p class="texte-intro"><?php echo $vars['user']['pseudo']; ?></p>
+
+        
     </div>
 </div>
 
 <div class="profil-zone3">
     <div class="container">
-        <div>
-            <a href="<?php echo WEBROOT . 'actions/users/update.php' ?>">Modifier mon profil</a>            
-            <a href="<?php echo WEBROOT . 'actions/users/delete.php' ?>">Supprimer mon compte</a>            
-        </div>
-        <div><?php echo 'Créé le ' . date_format(date_create($vars['user']['created']), 'd/m/Y à G\hi'); ?></div>
-        <ul>
-            <li class="" id="" name="" ><?php echo 'Pseudo : ' . $vars['user']['pseudo']; ?></li>
-            <li class="" id="" name="" ><?php echo 'Email : ' . $vars['user']['email']; ?></li>
-            <li class="" id="" name="" ><?php echo 'Prénom : ' . $vars['user']['firstname']; ?></li>
-            <li class="" id="" name="" ><?php echo 'Nom : ' . $vars['user']['lastname']; ?></li>
-            <li class="" id="" name="" ><?php echo 'Logo : ' . $vars['user']['logo']; ?></li>
-            <li class="" id="" name="" ><?php echo 'Adresse : ' . $vars['user']['number'] . ', ' . $vars['user']['street'] . '<br/>' . $vars['user']['postcode'] . ' ' . $vars['user']['city'] . '<br/>' . $vars['user']['country']; ?></li>
+        <ul class="speudo-email-utilisateur">
+            <li class="" id="" name="" ><?php echo $vars['user']['pseudo']; ?></li>
+            <li class="" id="" name="" ><?php echo $vars['user']['email']; ?></li>
+            <li><?php echo 'Créé le ' . date_format(date_create($vars['user']['created']), 'd/m/Y à G\hi'); ?></li>
         </ul>
+
+        <h2  class="h2-title-zone">Mes coordonées</h2>
+        <ul>
+
+            <li class="" id="" name="" >
+                <p>Nom :</p>
+                <p><?php echo $vars['user']['lastname']; ?></p>
+            </li>
+            <div class="clean"></div>
+            <li class="" id="" name="" >
+                <p>Prénom :</p>
+                <p><?php echo $vars['user']['firstname']; ?></p>
+            </li>
+            <div class="clean"></div>
+            <li class="" id="" name="" >
+                <p>Adresse :</p>
+                <p><?php echo $vars['user']['number'] . ', ' . $vars['user']['street']; ?></p>
+            </li>
+             <div class="clean"></div>
+            <li class="" id="" name="" >
+                <p>Ville :</p>
+                <p><?php echo $vars['user']['postcode'] . ', ' . $vars['user']['city']; ?></p>
+            </li>
+            <div class="clean"></div>
+            <li class="" id="" name="" >
+                <p>Pays :</p>
+                <p><?php echo $vars['user']['country']; ?></p>
+            </li>
+            <div class="clean"></div>
+        </ul>
+
+
+
+        <div class="btn-modif">
+            <a  class="btn" href="<?php echo WEBROOT . 'actions/users/update.php' ?>">Modifier mon profil</a>            
+            <a  class="btn" href="<?php echo WEBROOT . 'actions/users/delete.php' ?>">Supprimer mon compte</a>  
+                      
+        </div>
+        <p><?php echo 'Logo : ' . $vars['user']['logo']; ?></p>
+
+        
     </div>
 </div>
 

c33a4c57c056dd3304d5c302d8373df967ec1999
Goudouland <fanny.cayzeele@gmail.com>
2014-08-20 17:10:42 +0200

    ajout page 404.php

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/duck-city/phpconf/viewconf.php'; ?>
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/duck-city/header.php'; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require $_SERVER['DOCUMENT_ROOT'] . WEBROOT . 'footer.php'; ?>
+
+</body>
+</html>

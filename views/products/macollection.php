1a7f26b5e1ed7976d89645cbaf47d2c919469ab2
starkshadow <mystic_mistean@msn.com>
2014-08-22 23:21:27 +0200

    sfgfsgdfg

--- views/products/macollection.php
+++ views/products/macollection.php
@@ -11,22 +11,22 @@
     <div class="title-page">
         <h2 class="h2-title-page">Ma collection</h2>
     </div>
-
-
 </div>
 
 <div class="ma-collection-zone3 ">
     <div class="container">
         <div class="one-half column">
             <h2 class="h2-title-zone">Le dernier canard arrivé </br>dans ta collection</h2>
-            <h3 class="h3-nom-duck"><a href="#">Le duck cowboy</a></h3>
+            <h3 class="h3-nom-duck">
+                <a href="<?php echo WEBROOT . 'actions/products/details.php?id=' . $vars['lastproduct']['Product']['id']; ?>">
+                    <?php echo utf8_encode($vars['lastproduct']['Product']['name']); ?>
+                </a>
+            </h3>
 
             <div class="duck">
                 <img src="<?php echo WEBROOT . 'images/shop-galerie/duck-cowboy-p.png'; ?>">
             </div>
-            <div class="btn-ma-collection">
-                <a class="btn" href="achat.php">Tous mes achats</a>
-            </div>
+
         </div>
         <div class="one-half column">
             <h2 class="h2-title-zone">Peut-être le futur canard de ta collection ?</h2>
@@ -40,35 +40,15 @@
         </div>
     </div>
     <div class="container">
-        <h2 class="h2-title-zone h2-creation">Tes créations</h2>
+        <h2 class="h2-title-zone h2-creation">Tes achats</h2>
         <div class="one-third column">
+            <h3 class="h3-nom-duck"></h3>
+            <div class="duck">
+                <img src="<?php echo $boughtProducts['Product']['imgprofil']; ?>">
+            </div>
 
-            <h3 class="h3-nom-duck">Duck Geek</h3>
-            <div class="duck">
-                <img src="<?php echo WEBROOT . 'images/Duck-geek-profil.png'; ?>">
-            </div>
-            <div class="btn-ma-collection">
-                <a class="btn" href="">Acheter</a>
-            </div>
         </div>
-        <div class="one-third column">
-            <h3 class="h3-nom-duck">Duck sécurity</h3>
-            <div class="duck">
-                <img src="<?php echo WEBROOT . 'images/Duck-security-profil.png'; ?>">
-            </div>
-            <div class="btn-ma-collection">
-                <a class="btn" href="">Acheter</a>
-            </div>
-        </div>
-        <div class="one-third column">
-            <h3 class="h3-nom-duck">Duck vacancier diabolique</h3>
-            <div class="duck">
-                <img src="<?php echo WEBROOT . 'images/Duck-vacancier-diabolique-profil.png'; ?>">
-            </div>
-            <div class="btn-ma-collection">
-                <a class="btn" href="">Acheter</a>
-            </div>
-        </div>
+
     </div>
 
 </div>
